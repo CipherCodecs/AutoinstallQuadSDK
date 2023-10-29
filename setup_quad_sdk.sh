eeb9cf28298bf99cbf8814af0035c150bbf8f6a2
Shuo Chen <35624795+CipherCodecs@users.noreply.github.com>
2023-10-29 20:29:09 +0800

    setup_quad_sdk.sh
    Corrected the potential errors.Changed the issue-wrong source in the last step.

--- setup_quad_sdk.sh
+++ setup_quad_sdk.sh
@@ -1,3 +1,5 @@
+cd ~
+sudo rm -rf ./catkin_ws
 sudo apt install git
 source /opt/ros/melodic/setup.bash
 mkdir -p ~/catkin_ws/src
@@ -10,6 +12,11 @@ cd src
 git clone https://github.com/robomechanics/quad-sdk.git
 cd quad-sdk
 cp -r ~/Downloads/coinhsl ./external/ipopt/coinhsl # verify that quad-sdk/external/ipopt/coinhsl/ma27/ma27d.f is a valid path)
+sudo apt install python-pip
+sudo pip install -U rosdep
+sudo rosdep init
+rosdep update
 chmod +x setup.sh && ./setup.sh
 cd ~/catkin_ws
 catkin build
+source devel/setup.bash
